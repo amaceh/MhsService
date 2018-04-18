e245b4e69e3316e9337e89bee462ffbbd9f68381
amaceh <achmadar9i@gmail.com>
2018-04-18 23:49:14 +0700

    update Readme

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# Mahasiswa SLIM Web Service
+# Mahasiswa : SLIM Web Service
 
 Repository ini berisi contok kode sederhana untuk membuat web service yang mudah dan cepat. Service ini dibangun diatas [Slim Framework](https://www.slimframework.com/).
 
 Repository ini dibuat untuk membantu mahasiswa Mobile Programming untuk membuat projek mereka, tapi secara umum kode ini dapat dipakai oleh siapa saja.
+
+## What to do
+- Clone/Download Repository ini, Disain database baru, atau cukup import file sql yang sudah tersedia di phpmyadmin jika hanya ingin coba-coba dulu
+- Setting database terlebih dahulu di /src/settings.php sesuai setting db anda
+    ```
+    'host' => 'localhost',
+                'user' => 'root',
+                'pass' => '',
+                'dbname' => 'mhs_db',
+                'driver' => 'mysql'
+    ```
+ - Lalu modifikasi kode pada /src/routes.php sesuai dengan webservice yang kalian mau buat
+
+## Manual
+lihat di dokumen yang sudah [saya buat](https://docs.google.com/document/d/1AwS9_E2r0xfcHg6omcIFbsI73MKtUylwL7W8u3G0OGo/)
--- database.sql
+++ database.sql
@@ -1,16 +1,90 @@
-CREATE TABLE kuliah (
-    kode_mk INT NOT NULL AUTO_INCREMENT, 
-    nama_mk VARCHAR(100) NOT NULL, 
-    nama_dosen VARCHAR(100) NOT NULL, 
-    sks INT NOT NULL, 
-    PRIMARY KEY (`kode_mk`)
-) ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 18, 2018 at 11:47 PM
+-- Server version: 10.0.34-MariaDB-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
 
-CREATE TABLE users (
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(60) NOT NULL, 
-    api_key VARCHAR(64) NOT NULL,
-    PRIMARY KEY (`email`), 
-    UNIQUE (`api_key`)
-) ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mhs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kuliah`
+--
+
+CREATE TABLE `kuliah` (
+  `kode_mk` int(11) NOT NULL,
+  `nama_mk` varchar(100) NOT NULL,
+  `nama_dosen` varchar(100) NOT NULL,
+  `sks` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kuliah`
+--
+
+INSERT INTO `kuliah` (`kode_mk`, `nama_mk`, `nama_dosen`, `sks`) VALUES
+(1, 'Mobile Programming', 'Yudi WBS', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `email` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `api_key` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `password`, `api_key`) VALUES
+('madach@dulz.co', '123456789', 'suksesmobprog');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kuliah`
+--
+ALTER TABLE `kuliah`
+  ADD PRIMARY KEY (`kode_mk`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `api_key` (`api_key`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kuliah`
+--
+ALTER TABLE `kuliah`
+  MODIFY `kode_mk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
